be67a2b54b545cf9d8c6c1ff85f12075a724289b
Ayberk Levendoglu <ayberklevendoglu@gmail.com>
2024-01-05 01:02:30 +0300

    gogo

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -88,6 +88,17 @@ public class LinkedList {
         size--;
     }
 
+    public int[] toArray(){
+        int[] array = new int[size];
+        var current = first;
+        var index = 0;
+        while(current != null){
+            array[index++] = current.value;
+            current = current.next;
+        }
+        return array;
+    }
+
     public int size(){
         return size;
     }
